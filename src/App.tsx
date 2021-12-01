e97c2480a6aaa1d59cf39bc7024e4b3e72227b36
Akmal Alikhujaev <akmal@viaware.nl>
2021-12-01 20:21:17 +0100

    [CHANGE] Move Global Styles to another component
    Defined global utility classes for margin and padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
-import { createGlobalStyle } from 'styled-components';
 import LandingLayout from './components/layout/LandingLayout';
+import { GlobalStyle } from './components/ui/GlobalStyles';
 
 const App = () => {
-  const GlobalStyle = createGlobalStyle`
-    * {
-      font-family: var(--theme-font-family);
-      color: var(--theme-text-primary);
-    }
-
-    html {
-      font-size: var(--theme-font-size);  
-    }
-
-    body {
-      background: var(--theme-background-primary);
-    }
-  `;
   return (
     <>
       <GlobalStyle />
