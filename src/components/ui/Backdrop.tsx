075a6039ace181d2141f2df1a6627dbf43e078a9
akmal2409 <akmalalikhujaev@gmail.com>
2021-12-18 19:46:56 +0100

    [ADD] Custom backdrop component

---
+++ src/components/ui/Backdrop.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+const Backdrop = styled.div<StyleProps>`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  z-index: ${props => props.zIndex || '11'};
+  background-color: rgba(0,0,0,0.5);
+`;
+
+type StyleProps = {
+  zIndex?: number;
+}
+
+export default Backdrop;
